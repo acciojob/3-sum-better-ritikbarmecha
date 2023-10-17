0d1ae158280b854b99ad275f5e540b5936ec434b
ritikbarmecha <60346351+ritikbarmecha@users.noreply.github.com>
2023-10-17 14:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function threeSum(arr, target) {
+function threeSum(arr, sum) {
 // write your code here
+	let l,r;
+	arr.sort((a,b)=>a-b);
+     let n=arr.length;
+	for(let i=0;i<n-2;i++){
+		l=i+1;
+		r=n-1;
+		while(l<r){
+			if(arr[i]+arr[l]+arr[r]==sum){
+				document.write(arr[i]+" "+arr[l]+" "+arr[r]);
+			}else if(arr[i]+arr[l]+arr[r]<sum){
+				l++;
+			}else{
+				r--;
+			}
+		}
+	}
   
 }
 
